ab4743fdc1e45d321458c72e0626f4673ba4333a
Eduardo S Marques <edhana@gmail.com>
2012-03-08 14:57:51 -0300

    Adicionado o processo e os testes de assinatura

--- src/main/java/br/com/esec/estudo/TextSignee.java
+++ src/main/java/br/com/esec/estudo/TextSignee.java
@@ -1,22 +1,23 @@
 package br.com.esec.estudo;
 
 import java.security.*;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class TextSignee {
+  private static final String SIGNATURE_KEY = "DSA";
   private KeyPair keyPair;
 
   public TextSignee(){
+    // Configura o provider usado
+    Security.addProvider(new BouncyCastleProvider());
+
     this.keyPair = createKeyPair();
-  }
-
-  public TextSignee(KeyPair keyPair){
-
   }
 
   public KeyPair createKeyPair(){
     KeyPair kp = null;
     try{
-      KeyPairGenerator generator = KeyPairGenerator.getInstance("DSA");
+      KeyPairGenerator generator = KeyPairGenerator.getInstance(SIGNATURE_KEY);
       SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
       random.setSeed(System.currentTimeMillis());
       generator.initialize(1024, random);
@@ -28,6 +29,43 @@ public class TextSignee {
     }
 
     return kp;
+  }
+
+  public byte[] sign(String textToSign){
+    byte signedText[] = null;
+
+    try{
+      Signature signature = Signature.getInstance(SIGNATURE_KEY);
+      signature.initSign(this.keyPair.getPrivate());
+      signature.update(textToSign.getBytes());
+      signedText = signature.sign();
+    }catch(NoSuchAlgorithmException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }catch(InvalidKeyException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }catch(SignatureException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }
+
+    return signedText;
+  }
+
+  public boolean verify(String originalText, byte [] signedText){
+    try{
+      Signature signature = Signature.getInstance(SIGNATURE_KEY);
+      signature.initVerify(this.keyPair.getPublic());
+      signature.update(originalText.getBytes());
+      return signature.verify(signedText);      
+    }catch(NoSuchAlgorithmException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }catch(InvalidKeyException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }catch(SignatureException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }
+
+
+    return false;
   }
 
   public KeyPair getKeyPair(){
--- src/test/java/br/com/esec/test/TextSigneeTest.java
+++ src/test/java/br/com/esec/test/TextSigneeTest.java
@@ -5,26 +5,43 @@ import static org.junit.Assert.*;
 import br.com.esec.estudo.TextSignee;
 
 public class TextSigneeTest{
+  public TextSignee signee;
+  public String textToSign;
   
+  @Before
+  public void setUp(){
+    signee = new TextSignee();
+    textToSign = "So vejo bachareis a minha volta.";
+  }
+
   @Test
   public void test_shouldCreateDefaultTextSignee(){
     System.out.println("Should create the default text signee ...");
-    TextSignee signee = new TextSignee();
     assertNotNull(signee);
   }
 
   @Test
   public void test_shouldCreateKeyPain(){
     System.out.println("Should create a keyPair ...");
-    TextSignee signee = new TextSignee();
     assertNotNull(signee.createKeyPair());
   }
 
   @Test
   public void test_shouldCreateAKeyPairInDefaultConstructor(){
     System.out.println("Should create a keyPair object when using the default constructor ...");
-    TextSignee signee = new TextSignee();
     assertNotNull(signee.getKeyPair()); 
+  }
+
+  @Test
+  public void test_shouldSignAText(){
+    System.out.println("Should sign a random text ...");
+    assertNotNull(signee.sign(textToSign));
+  }
+
+  @Test
+  public void test_shouldValidateASignedText(){
+    System.out.println("Should validate a sign on a random text ...");
+    assertTrue(signee.verify(textToSign, signee.sign(textToSign)));
   }
   
 }
