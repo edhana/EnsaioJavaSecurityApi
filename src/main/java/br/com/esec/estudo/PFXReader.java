90f095a9b4d1776909a8680a17b53087cb6c4247
Eduardo S Marques <edhana@gmail.com>
2012-03-09 16:16:02 -0300

    Acertei o keystore e adicinei um novo metodo para pegar o alias que ainda esta sem teste [shame on me]

--- src/main/java/br/com/esec/estudo/PFXReader.java
+++ src/main/java/br/com/esec/estudo/PFXReader.java
@@ -2,13 +2,67 @@ package br.com.esec;
 
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.FileNotFoundException;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
+
+import java.util.Enumeration;
 
 public class PFXReader{
   public InputStream openPFXFile(String path){
     try{
       return new FileInputStream(path);
     }catch(FileNotFoundException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }
+
+    return null;
+  }
+
+  public KeyStore getKeyStore(InputStream file) throws FileNotFoundException{
+    try{
+      if (file != null){
+        KeyStore ks = KeyStore.getInstance("PKCS12", "BC");
+        return ks;
+      }
+      else{
+        // TODO: Refatorar para uma classe de exception propria
+        throw new FileNotFoundException("A .pfx file must be informed in order to retrieve the KeyStore");
+      }
+    }catch(KeyStoreException e){
+      System.out.println("====> Excepition: " + e.getMessage()); 
+    }catch(NoSuchProviderException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }
+
+    return null;
+  }
+
+  /**
+   * Return the unique alias of a keystore
+   */
+  public String getKeyEntry(String filename){
+    
+    try{
+      InputStream file = openPFXFile(filename);
+      KeyStore ks = getKeyStore(file);
+      String password = "1234";
+      ks.load(file, password.toCharArray());
+      Enumeration<String> en = ks.aliases();
+      String alias = (String)en.nextElement();
+      return alias;
+    }catch(IOException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }catch(KeyStoreException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }catch(NoSuchAlgorithmException e){
+      System.out.println("====> Excepition: " + e.getMessage());
+    }catch(CertificateException e){
       System.out.println("====> Excepition: " + e.getMessage());
     }
 
--- src/test/java/br/com/esec/test/PFXReaderTest.java
+++ src/test/java/br/com/esec/test/PFXReaderTest.java
@@ -4,13 +4,33 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import br.com.esec.PFXReader;
 
+import java.io.FileNotFoundException;
+
 public class PFXReaderTest{
 
+  private PFXReader reader;
+
+  @Before
+  public void setUp(){
+    reader = new PFXReader();
+  }
+  
   @Test 
   public void test_shouldOpenThePFXFile(){
     System.out.println("Should open the project pfx file ... ");
-    PFXReader reader = new PFXReader();
     assertNotNull(reader.openPFXFile("openssl/mycert.pfx"));
+  }
+
+  @Test
+  public void test_shouldRetrieveTheKeyStoreFromFile(){
+    System.out.println("Should retrieve KeyStore from pfx file ... ");
+
+    // Retrieve KeyEntry Object
+    try{
+      assertNotNull(reader.getKeyStore(reader.openPFXFile("openssl/mycert.pfx")));      
+    }catch(FileNotFoundException e){
+      fail();
+    }
   }
 
 }
